fffe98df426f2ce7742c5dac9d2bb7eafe2fa7b5
tkmagesh <tkmagesh77@gmail.com>
2014-04-09 16:12:59 +0530

    seed code for functional programming

---
+++ Day-1/JavaScript/FunctionalProgramming/products.js
@@ -0,0 +1,8 @@
+var products = [
+	{id : 7, name : "pen", cost : 20, units : 10, category : 1},
+	{id : 2, name : "hen", cost : 10, units : 80, category : 2},
+	{id : 6, name : "ten", cost : 30, units : 20, category : 1},
+	{id : 5, name : "len", cost : 10, units : 70, category : 2},
+	{id : 4, name : "den", cost : 20, units : 50, category : 1},
+	{id : 1, name : "zen", cost : 14, units : 30, category : 2}
+]
