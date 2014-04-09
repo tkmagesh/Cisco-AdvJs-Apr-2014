ca6c9ee2d533c3337c6f4613e97079c3bc472ac4
tkmagesh <tkmagesh77@gmail.com>
2014-04-09 15:40:08 +0530

    Closures Demo

--- Day-1/JavaScript/isPrime.js
+++ Day-1/JavaScript/isPrime.js
@@ -1,16 +1,36 @@
-function isPrime(n){
-	if (typeof isPrime.cache === "undefined")
-		isPrime.cache = {0 : true, 1 : true, 2 : true, 3 : true};
-	if (typeof isPrime.cache[n] !== "undefined"){
-		console.log("Returing from cache");
-	} else {
-		console.log("Freshly brewed...")
-		isPrime.cache[n] = true;
-		for(var i=0;i<=(n/2);i++){
-			if (n % i === 0) {
-				isPrime.cache[n] = false;
+var isPrime = (function(){
+	var cache = {0 : true, 1 : true, 2 : true, 3 : true};
+	return function(n){
+		if (typeof cache[n] !== "undefined"){
+			console.log("Returing from cache");
+		} else {
+			console.log("Freshly brewed...")
+			cache[n] = true;
+			for(var i=0;i<=(n/2);i++){
+				if (n % i === 0) {
+					cache[n] = false;
+				}
 			}
 		}
+		return cache[n];
 	}
-	return isPrime.cache[n];
-}
+})()
+
+function isPrime(n){
+	var cache = {0 : true, 1 : true, 2 : true, 3 : true};
+	isPrime = function(n){
+			if (typeof cache[n] !== "undefined"){
+				console.log("Returing from cache");
+			} else {
+				console.log("Freshly brewed...")
+				cache[n] = true;
+				for(var i=0;i<=(n/2);i++){
+					if (n % i === 0) {
+						cache[n] = false;
+					}
+				}
+			}
+			return cache[n];
+		}
+	return isPrime(n);
+};
